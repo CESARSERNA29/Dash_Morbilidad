6d0a5c6155b50239525c8686559d14ec1e777948
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-06-23 22:19:23 -0500

    Add files via upload

--- Home_Tablero.py
+++ Home_Tablero.py
@@ -11,7 +11,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
-from numerize.numerize import numerize
+# from numerize.numerize import numerize
+from numerize import numerize
+
 import time
 from streamlit_extras.metric_cards import style_metric_cards
 # st.set_option('deprecation.showPyplotGlobalUse', False)
