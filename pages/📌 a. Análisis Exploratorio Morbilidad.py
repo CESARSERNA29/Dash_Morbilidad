eb63a2a39636e3712d47c9c735e1d3bad77ed01f
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-07-03 01:09:38 -0500

    Add files via upload

--- pages/📌 a. Análisis Exploratorio Morbilidad.py
+++ pages/📌 a. Análisis Exploratorio Morbilidad.py
@@ -4,24 +4,24 @@
 
 
 # Cargando las Librerías:
-    
+# ======================
+
 import streamlit as st
 import pandas as pd
 # from pandas_profiling import ProfileReport
 import streamlit.components.v1 as components
-
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
 # from numerize.numerize import numerize
 from numerize import numerize
-
 import time
 from streamlit_extras.metric_cards import style_metric_cards
 # st.set_option('deprecation.showPyplotGlobalUse', False)
 import plotly.graph_objs as go
+# ----------------------------------------------------------
+
 
 
 # Descomenta esta línea si usas MySQL:
@@ -309,40 +309,7 @@ header {visibility:hidden;}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+st.markdown("##")
 
 
 
